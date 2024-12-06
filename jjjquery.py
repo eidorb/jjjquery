6ec9d2372fb6b314ef778063b6d829d358cae238
eidorb <brodieface@gmail.com>
2024-12-06 16:03:27 +1000

    Print latest song

--- jjjquery.py
+++ jjjquery.py
@@ -1,2 +1,11 @@
 """Triple J-Query!"""
-print(__doc__)
+
+from pprint import pp
+
+from abc_radio_wrapper import ABCRadio
+
+if __name__ == "__main__":
+    abc_radio = ABCRadio()
+    for radio_song in abc_radio.search().radio_songs:
+        pp(radio_song)
+        break  # for now
