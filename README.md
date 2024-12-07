f3473d539b43c6e9c315cc7ce662f58921c99e12
eidorb <brodieface@gmail.com>
2024-12-07 14:21:38 +1000

    Add marimo notebook
    - update readme with link to static marimo app

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Triple J-Query 🥁
 
 ![](image.jpeg)
 
+Check out a read-only version of the [`jjjquery.py`](jjjquery.py) notebook on the web! https://static.marimo.app/static/jjjquery-7tix
+
 
 ## Roadmap
 
-- [ ] Scrape recently played from https://www.abc.net.au/triplej/live/triplej
+- [x] Scrape recently played from https://www.abc.net.au/triplej/live/triplej
 - [ ] Do this automatically using Git scraping https://simonwillison.net/2020/Oct/9/git-scraping/
 - [ ] Front-end the data with Datasette https://datasette.io
 - [ ] Build a custom interface
--- jjjquery.py
+++ jjjquery.py
@@ -1,11 +1,185 @@
-"""Triple J-Query!"""
+import marimo
 
-from pprint import pp
+__generated_with = "0.9.31"
+app = marimo.App(width="medium", app_title="jjjquery")
 
-from abc_radio_wrapper import ABCRadio
+
+@app.cell(hide_code=True)
+def __():
+    from datetime import datetime, timedelta
+
+    import marimo as mo
+
+
+    from_ = mo.ui.datetime(value=datetime(year=2024, month=1, day=1))
+
+    mo.md(
+        f"""
+        # jjjquery 🥁 Triple J-Query
+
+        {mo.image('https://raw.githubusercontent.com/eidorb/jjjquery/refs/heads/main/image.jpeg', rounded=True).center()}
+
+        It looks like it's simple to get started with [ABC Radio Wrapper](https://github.com/MatthewBurke1995/ABC-Radio-Wrapper).
+
+        Let's search for the first song played from the beginning of the new year (in UTC) {from_}.
+        """
+    )
+    return datetime, from_, mo, timedelta
+
+
+@app.cell
+def __(from_, mo, timedelta):
+    from dataclasses import asdict
+
+    from abc_radio_wrapper import ABCRadio
+
+    abc_radio = ABCRadio()
+    to = from_.value + timedelta(minutes=1)
+    for search_result in abc_radio.continuous_search(from_=from_.value, to=to):
+        for first_radio_song in search_result.radio_songs:
+            continue
+
+    mo.md(
+        f"""
+        Search parameters:
+        {mo.as_html(dict(from_=from_.value, to=to))}
+
+        First played song: 
+        {mo.as_html(asdict(first_radio_song))}
+        """
+    )
+    return ABCRadio, abc_radio, asdict, first_radio_song, search_result, to
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""Can we style the song?""")
+    return
+
+
+@app.cell
+def __(first_radio_song, mo):
+    mo.output.append(
+        mo.hstack(
+            [
+                mo.image(first_radio_song.song.album.artwork.url),
+                mo.md(
+                    rf"""
+                    **{first_radio_song.song.title}**  
+                    by _{', '.join(artist.name for artist in first_radio_song.song.artists)}_  
+                    from _{first_radio_song.song.album.title}_  
+                    played at {first_radio_song.played_time:%c}  
+                    on `{first_radio_song.channel}`
+                    """
+                ),
+            ],
+            justify="start",
+            align="start",
+        )
+    )
+    return
+
+
+@app.cell
+def __(mo):
+    minutes_slider = mo.ui.slider(start=5, stop=90, step=5, value=30, debounce=True, show_value=True)
+    mo.md(
+        f"""
+        Can we mimic the Triple J [live player](https://www.abc.net.au/triplej/live/triplej)
+        and show what was played in the last {minutes_slider} minutes?
+        """
+    )
+    return (minutes_slider,)
+
+
+@app.cell(hide_code=True)
+def __(abc_radio, datetime, minutes_slider, mo, timedelta):
+    from datetime import timezone
+
+    now = datetime.now(timezone.utc)
+    recently_played = [
+        radio_song
+        for search_result in abc_radio.continuous_search(
+            from_=now - timedelta(minutes=minutes_slider.value), to=now, station="triplej", limit=100
+        )
+        for radio_song in search_result.radio_songs
+    ]
+
+    # Style the latest song differently.
+    mo.output.append(
+        mo.vstack(
+            [
+                mo.audio(
+                    "https://mediaserviceslive.akamaized.net/hls/live/2038308/triplejnsw/index.m3u8"
+                ),
+                mo.md(
+                    '<span style="color:white">'
+                    + f"{recently_played[0].played_time.astimezone(timezone(timedelta(hours=10))):%I:%M %p}".lstrip(
+                        "0"
+                    )
+                    + "</span>"
+                ),
+                mo.hstack(
+                    [
+                        mo.image(src=recently_played[0].song.album.artwork.sizes[0].url),
+                        mo.md(
+                            f"""
+                            **<span style="color:white">{recently_played[0].song.title}</span>**
+
+                            <span style="color:white">{recently_played[0].song.artists[0].name}</span>  
+                            <span style="color:white">{recently_played[0].song.album.title}</span>
+                            """
+                        ),
+                    ],
+                    justify="start",
+                    align="start",
+                ),
+            ]
+        )
+        .callout()
+        .style({"background-color": "#333"})
+        .center()
+    )
+
+    # Display the remaining songs.
+    for radio_song in recently_played[1:]:
+        mo.output.append(
+            mo.vstack(
+                [
+                    mo.vstack(
+                        [
+                            mo.md(
+                                f"{radio_song.played_time.astimezone(timezone(timedelta(hours=10))):%I:%M %p}".lstrip(
+                                    "0"
+                                ),
+                            ),
+                            mo.hstack(
+                                [
+                                    mo.image(
+                                        src=radio_song.song.album.artwork.sizes[0].url
+                                        if radio_song.song.album
+                                        else ""
+                                    ),
+                                    mo.md(
+                                        f"""
+                                **{radio_song.song.title}**
+
+                                {", ".join(artist.name for artist in radio_song.song.artists)}  
+                                {radio_song.song.album.title if radio_song.song.album else ""}  
+                                [YouTube](https://www.youtube.com/results?search_query={" ".join(artist.name for artist in radio_song.song.artists)} {radio_song.song.title})
+                                """
+                                    ),
+                                ],
+                                justify="start",
+                            ),
+                        ],
+                        align="start",
+                    ).callout()
+                ]
+            ).center()
+        )
+    return now, radio_song, recently_played, timezone
+
 
 if __name__ == "__main__":
-    abc_radio = ABCRadio()
-    for radio_song in abc_radio.search().radio_songs:
-        pp(radio_song)
-        break  # for now
+    app.run()
